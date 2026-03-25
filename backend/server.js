06884c9b7603e7eb38ba52008725b30e006dee5b
Rakesh Rock <25bcad59@kristujayanti.com>
2026-03-25 07:21:46 +0530

    change name

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
-app.get("/products",(req,res)=>{
- res.json([
-  {id:1,name:"Laptop",price:800},
-  {id:2,name:"Phone",price:400}
- ]);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "flower",
+  database: "webshop"
 });
 
-app.listen(3000,()=>{
- console.log("Server running on port 3000");
+app.get("/products", (req, res) => {
+  db.query("SELECT * FROM products", (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
 });
+
+app.listen(3000, () => console.log("Server running"));
