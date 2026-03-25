5e9cc202296606946aa60536ec22874e34446b38
Rakesh Rock <25bcad59@kristujayanti.com>
2026-03-25 07:24:22 +0530

    change name

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-const products = [
- {id:1, name:"Laptop", price:800},
- {id:2, name:"Phone", price:400},
- {id:3, name:"Headphones", price:100}
-];
+fetch("http://localhost:3000/products")
+  .then(response => response.json())
+  .then(data => {
 
-const container = document.getElementById("products");
+    const container = document.getElementById("products");
 
-products.forEach(p=>{
- container.innerHTML += `
-  <div>
-   <h3>${p.name}</h3>
-   <p>$${p.price}</p>
-   <button onclick="addToCart('${p.name}')">Add to Cart</button>
-  </div>
- `;
-});
+    data.forEach(product => {
+      container.innerHTML += `
+        <div>
+          <h3>${product.name}</h3>
+          <p>$${product.price}</p>
+        </div>
+      `;
+    });
 
-function addToCart(product){
- alert(product + " added to cart");
-}
+  })
+  .catch(error => console.log(error));
